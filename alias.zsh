c3c89f916d4cac96b65cb221cea5dfbb1432105c
xztaityozx <taityonohibi@gmail.com>
2018-09-29 10:14:41 +0900

    update alias

--- alias.zsh
+++ alias.zsh
@@ -1,11 +1,11 @@
 #!bin/zsh
 
-type exa &> /dev/null && alias ls=exa
+type exa &> /dev/null && alias ls="exa --git"
 type libinput-gestures-setup &> /dev/null && alias gesrestart="libinput-gestures-setup restart"
 alias rescale="$HOME/.ghq/github.com/xztaityozx/dotfiles/scaleSet.sh"
 alias millitime="$HOME/.ghq/github.com/xztaityozx/dotfiles/millitime.sh"
 [[ -f "$HOME/.utils/prettyping" ]] && alias ping="$HOME/.utils/prettyping --nolegend"
-type bat &> /dev/null && alias cat=bat
+type bat &> /dev/null && alias cat="bat --theme TwoDark"
 [[ -f "$HOME/.utils/diff-so-fancy" ]] && alias diff="$HOME/.utils/diff-so-fancy"
 [[ -f $HOME/.utils/nvim.appimage ]] && alias nvim="$HOME/.utils/nvim.appimage" && alias vim="$HOME/.utils/nvim.appimage"
 
