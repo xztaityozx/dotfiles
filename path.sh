cd8417031da856b1fd4db670cc4da55bf780c6c2
xztaityozx <taityonohibi@gmail.com>
2019-04-12 12:09:49 +0900

    update

--- path.sh
+++ path.sh
@@ -1,2 +1,3 @@
 [ -f $HOME/utils/GoLand-2018.2.3/bin/goland.sh ] && export PATH="$PATH:$HOME/utils/GoLand-2018.2.3/bin"
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+export fpath=($fpath /home/linuxbrew/.linuxbrew/Homebrew/completions/zsh /home/linuxbrew/.linuxbrew/share/zsh/site-functions)
