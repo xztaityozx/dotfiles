8dc2588be36ce84be7f46a37cb86f825beb19e7d
xztaityozx <taityonohibi@gmail.com>
2019-06-04 23:51:12 +0900

    update

--- ghq-get.zsh
+++ ghq-get.zsh
@@ -6,5 +6,3 @@ mkdir -p ~/.config/tilix/schemes
 cp ~/.ghq/github.com/storm119/Tilix-Themes/Themes/* ~/.config/tilix/schemes/
 cp ~/.ghq/github.com/storm119/Tilix-Themes/Themes-2/* ~/.config/tilix/schemes/
 
-ghq get saibing/bingo.git
-cd ~/.ghq/github.com/saibing/bingo && GO111MODULE=on go install
