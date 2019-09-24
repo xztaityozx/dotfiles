a23b928b6d09f3c56a291b52bbb081a1f1c45062
xztaityozx <taityonohibi@gmail.com>
2019-09-24 10:46:23 +0900

    add function yy()

--- function.zsh
+++ function.zsh
@@ -34,8 +34,44 @@ function hub-remote-set-ssh() {
   hub remote set-url origin git@github.com:xztaityozx/$repo
 }
 
+# generate password
+# usage: 
+#   - gen-password [number of passwords] [length of password]
 function gen-passwd(){
   local cnt=${2:-10}
   local w=${1:-10}
   cat /dev/urandom|base64|head -n5| fold -w$w | head -n$cnt
 }
+
+# output information to stdout
+# usage: logger.info [text]
+function logger.info() {
+  echo "\033[34m[info] [$(date)]\033[39m $*"
+}
+
+# output warning to stdout
+# usage: logger.warn [text]
+function logger.warn() {
+  echo "\033[33m[warn] [$(date)]\033[39m $*"
+}
+
+# copy text to clipboard
+# usage:
+#   - yy [text] -> copy [text] to clipboard
+#   - [command] | yy -> copy [command]'s output to clipboard
+#   - yy -> copy last command
+function yy() {
+  local text="${*}"
+  [ -p /dev/fd/0 ] && text=$(cat -)
+  [ "$text" = "" ] && text="$(history | tail -n1 |cut -d' ' -f4-)"
+
+  # wsl
+  type clip.exe 2>&1 > /dev/null && echo -n "$text" | clip.exe && return 0
+  # xclip
+  type xclip 2>&1 > /dev/null && echo -n "$text" | xclip -selection c return 0
+  # xsel 
+  type xsel 2>&1 > /dev/null && echo -n "$text" | xsel -b && return 0
+
+  logger.warn "clip.exe, xclip or xsel not found"
+}
+
