32f7b670810244be972c9604c51e2ff4cbeb8eec
xztaityozx <taityonohibi@gmail.com>
2020-01-05 22:37:42 +0900

    update zshrc

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dotfiles
+
+```sh
+git clone https://github.com/xztaityozx/dotfiles
+cd dotfiles
+bash ./install.sh
+```
--- ln.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/zsh
-
-REPO="$HOME/.ghq/github.com/xztaityozx/dotfiles/"
-
-ln -s $REPO/config/* $HOME/.config/
-ln -s $REPO/zshrc $HOME/.zshrc
-ln -s $REPO/zshenv $HOME/.zshenv
-ln -s $REPO/ideavimrc $HOME/.ideavimrc
