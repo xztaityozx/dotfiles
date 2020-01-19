7e6f7b06e9fdec75563cee367ec3a4526d72bf1d
xztaityozx <taityonohibi@gmail.com>
2020-01-19 11:38:43 +0900

    add alias

--- config/zsh/alias.zsh
+++ config/zsh/alias.zsh
@@ -14,6 +14,7 @@ alias ka=ls
 alias cg="cdx -c g"
 alias ch="cdx -h"
 alias cb="cdx -b"
+alias ..="cdx ../"
 
 # git
 [ -f "$(brew --prefix)/bin/git" ] && {
@@ -29,7 +30,14 @@ alias hps="hub push"
 alias hub-update="hub add -A && hub commit -m 'update' && hub push"
 alias hsw="hub switch"
 
+# fcitx
+alias fcitx-autostart="fcitx-autostart &> /dev/null"
+
 # for touchpad gesture
 alias gesrestart="libinput-gestures-setup restart"
 
+# go
+alias -s go="go run "
+
+# windows terminal
 [ "$WT_SESSION" != "" ] && alias open-wterm-settiong="nvim /home/xztaityozx/winhome/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/profiles.json"
