3cc4ae789d0335776d7417cd5e207f8d96224466
xztaityozx <taityonohibi@gmail.com>
2020-05-24 12:28:23 +0900

    めんどくせえな

--- install.sh
+++ install.sh
@@ -22,10 +22,10 @@ type pacman &> /dev/null && {
   sudo pacman -Ss git go python3 zsh
 }
 
-mkdir $HOME/.local/share/nvim/site/autoload
+mkdir -p $HOME/.local/share/nvim/site/autoload
 
 # zinit
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 
 ln -s $SCRIPT_DIR/config/* $HOME/.config/
 ln -s $SCRIPT_DIR/ideavimrc $HOME/.ideavimrc
