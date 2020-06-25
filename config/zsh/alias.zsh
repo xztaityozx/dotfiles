d8b42dc5230338a5ac456a11d7a3322453e143e7
xztaityozx <taityonohibi@gmail.com>
2020-06-25 15:52:28 +0900

    catfを更新

--- config/zsh/alias.zsh
+++ config/zsh/alias.zsh
@@ -6,7 +6,7 @@ type bat &> /dev/null && alias cat="bat --theme TwoDark"
 # {{{
   # $_.cat とすれば、直前にlsとかnvimしたファイルをcatできる。あんまつかわん
   alias -s cat='(){cat ${1/.cat/}}'
-  alias catf="(){tail -f ${@} | cat -l log --paging=never}"
+  alias catf="(){tail -f \${@} | cat -l log --paging=never}"
   alias fcat="fzf | :awk '{print \$1}' | icat"
 # }}}
 type hexyl &> /dev/null && alias xxd=hexyl
