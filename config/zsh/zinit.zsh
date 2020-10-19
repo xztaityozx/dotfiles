fea0ee15d37ac81cc8de78bd150c04e9422d2706
xztaityozx <taityonohibi@gmail.com>
2020-10-20 08:32:33 +0900

    has"tilix"

--- config/zsh/zinit.zsh
+++ config/zsh/zinit.zsh
@@ -32,7 +32,7 @@ zinit has"go" atclone"go install" atpull"%atclone" as"null" for \
 
 zinit cloneonly as"null" for \
   cp"plug.vim -> $HOME/.local/share/nvim/site/autoload/plug.vim" junegunn/vim-plug \
-  cp"./*/*.json -> $ENV_DOT_CONFIG/tilix/schemes"                storm119/Tilix-Themes
+  has"tilix" cp"./*/*.json -> $ENV_DOT_CONFIG/tilix/schemes"     storm119/Tilix-Themes
 
 
 zinit as"program" from"gh-r" for \
