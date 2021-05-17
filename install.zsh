40d5c4c66a7e522c210ae30814fcff1f0b1b1a4f
xztaityozx <taityonohibi@gmail.com>
2021-05-17 11:21:39 +0900

    xenv系のzinit調整

--- install.zsh
+++ install.zsh
@@ -8,10 +8,17 @@ type apt &> /dev/null && {
 
   sudo apt update && sudo apt upgrade -y
   sudo apt install -y build-essential wget curl git moreutils
+  # pyenv
+  sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+                            libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+                            libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 }
 
 type brew &> /dev/null && {
   brew install git zsh moreutils
+
+  # pyenv
+  brew install openssl readline sqlite3 xz zlib
 }
 
 type pacman &> /dev/null && {
@@ -24,6 +31,7 @@ mkdir -p $HOME/.local/share/nvim/site/autoload
 mkdir -p $SCRIPT_DIR/config/zsh/.zinit
 git clone https://github.com/zdharma/zinit.git $SCRIPT_DIR/config/zsh/.zinit/bin
 
+mkdir -p $HOME/.config
 ln -s $SCRIPT_DIR/config/* $HOME/.config/
 ln -s $SCRIPT_DIR/ideavimrc $HOME/.ideavimrc
 
