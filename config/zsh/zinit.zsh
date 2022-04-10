8dd456dadac5b756691a7406975a0a267a3ab626
xztaityozx <taityonohibi@gmail.com>
2022-04-10 19:48:21 +0900

    binの方はシンボリックリンクでした

--- config/zsh/zinit.zsh
+++ config/zsh/zinit.zsh
@@ -37,7 +37,7 @@ type zinit &> /dev/null && {
       true
     }
 
-    zinit ice lbin'!bin/anyenv' atclone"yes | bin/anyenv install --init" eval"bin/anyenv init - zsh" \
+    zinit ice lbin'!libexec/anyenv' atclone"yes | bin/anyenv install --init" eval"bin/anyenv init - zsh" \
       atload'export ANYENV_ROOT=$PWD;_zinit_anyenv_atload' \
       atpull"anyenv install --update"
     zinit light anyenv/anyenv
