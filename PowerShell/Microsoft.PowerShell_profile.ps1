e242481a118c7a6c1784a0652c27f3782041de37
xztaityozx <taityonohibi@gmail.com>
2023-03-05 22:15:51 +0900

    最低限のPROFILE

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,59 +1,38 @@
-
-# PowerLine setting
-Import-Module ((ghq root)+"\github.com\xztaityozx\PowerShell-Dotfiles\Get-PowerLinePath.ps1")
-Import-Module posh-git
-Import-Module oh-my-posh
-
 # kye-bind
 Import-Module PSReadLine
 Set-PSReadLineOption -EditMode Emacs -BellStyle None
 Set-PSReadLineKeyHandler -Chord Ctrl+D -ScriptBlock { [System.Environment]::Exit(0) }
+Set-PSReadLineKeyHandler -Chord Ctrl+LeftArrow BackwardWord
+Set-PSReadLineKeyHandler -Chord Ctrl+RightArrow ForwardWord
+Set-PsFzfOption -TabExpansion -PSReadlineChordReverseHistory 'Ctrl+r'
+Set-PSReadLineKeyHandler -Chord Tab -ScriptBlock {Invoke-FzfTabCompletion}
+
 
 # environment
 $env:NVIM_CONFIG_DIR="C:\Users\xztaityozx\.ghq\github.com\xztaityozx\dotfiles\config\nvim\"
-$env:PYTHON2_HOST_PROG="C:\Python27\python.exe"
 
-# theme
-Set-Theme Craft
-
-# sudo
-function Pause {
-    if ($psISE) {
-        $null = Read-Host 'Press Enter Key...'
-    }
-    else {
-        Write-Host "Press Any Key..."
-        (Get-Host).UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null
-    }
+function global:prompt {
+    $startInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $startInfo.FileName = "powerline-go"
+    $startInfo.Arguments = "-shell bare -modules ssh,host,docker,cwd,git,jobs,exit,newline,user,root -cwd-max-depth 3 -cwd-max-dir-size -1 -hostname-only-if-ssh"
+    $startInfo.Environment["TERM"] = "xterm-256color"
+    $startInfo.CreateNoWindow = $true
+    $startInfo.StandardOutputEncoding = [System.Text.Encoding]::UTF8
+    $startInfo.RedirectStandardOutput = $true
+    $startInfo.UseShellExecute = $false
+    $startInfo.WorkingDirectory = $pwd
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $startInfo
+    $process.Start() | Out-Null
+    $standardOut = $process.StandardOutput.ReadToEnd()
+    $process.WaitForExit()
+    $standardOut
 }
 
-function Invoke-CommandRunAs {
-    $cd=(Get-Location).Path
-    $commands="Set-Location $cd; Write-Host `"[Administrator] $cd> $args`"; $args; Pause; exit"
-    $bytes=[System.Text.Encoding]::Unicode.GetBytes($commands)
-    $encodedCommand=[System.Convert]::ToBase64String($bytes)
-    Start-Process powershell.exe -Verb RunAs -ArgumentList "-NoExit","-encodedCommand",$encodedCommand
-}
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell | Out-String)
+})
 
-Set-Alias sudo Invoke-CommandRunAs
+Set-Alias :e nvim
 
-function Start-RunAs {
-    $cd=(Get-Location).Path
-    $commands="Set-Location $cd; (Get-Host).UI.RawUI.WindowTitle+=`" [Administrator] `""
-    $bytes=[System.Text.Encoding]::Unicode.GetBytes($commands)
-    $encodedCommand=[System.Convert]::ToBase64String($bytes)
-    Start-Process powershell.exe -Verb RunAs -ArgumentList "-NoExit","-encodedCommand",$encodedCommand
-}
-
-Set-Alias su Start-RunAs
-
-function hac {
-    param (
-        [string]$message
-    )
-    
-    hub add -A
-    hub commit -m $message
-}
-
-Set-Alias hps "hub push"
