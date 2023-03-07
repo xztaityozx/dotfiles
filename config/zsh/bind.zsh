975a02d33733ac85de9a9a362f39d3c1b98abcca
xztaityozx <taityonohibi@gmail.com>
2023-03-07 10:17:05 +0900

    使ってないバインドを削除

--- config/zsh/bind.zsh
+++ config/zsh/bind.zsh
@@ -7,14 +7,6 @@ bindkey "^[[3~" delete-char
 bindkey "^[[1~" beginning-of-line
 bindkey "^[[4~" end-of-line
 
-__insert_awk__(){
-  local TEXT="awk '{}'"
-  BUFFER=${BUFFER:0:$CURSOR}$TEXT${BUFFER:$CURSOR}
-  CURSOR=$(($CURSOR+6))
-}
-zle -N __insert_awk__
-bindkey "^Xawk" __insert_awk__
-
 __edit_line__(){
   local FILE="/tmp/$(date "+%s")line.sh"
   echo "$BUFFER" > $FILE &&
@@ -26,26 +18,3 @@ __edit_line__(){
 zle -N __edit_line__
 bindkey "^Xe" __edit_line__
 
-__local_branch_widget__() {
-  local result="$(hub branch | sed 's/[ *]*//g' | fzf)"
-  local ret=$?
-  zle reset-prompt
-  BUFFER=${BUFFER}" "${result}
-  CURSOR=${#BUFFER}
-  return $ret
-}
-
-zle -N __local_branch_widget__
-bindkey "^Xb" __local_branch_widget__
-
-__remote_branch_widget__() {
-  local result="$(hub branch --remotes | sed 's/[ *]*//g' | fzf)"
-  local ret=$?
-  zle reset-prompt
-  BUFFER=${BUFFER}" "${result}
-  CURSOR=${#BUFFER}
-  return $ret
-}
-
-zle -N __remote_branch_widget__
-bindkey "^XB" __remote_branch_widget__
