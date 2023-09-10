c7f8b8249582edf145ab293c09fd1f8f1dc4c3cb
xztaityozx <taityonohibi@gmail.com>
2023-09-10 14:41:21 +0900

    iceいらない

--- config/zsh/zinit.common.zsh
+++ config/zsh/zinit.common.zsh
@@ -9,7 +9,7 @@
 
   # nvim
   # zinitにrelaeseの名前を解決させることができないのでなんとかする
-  zinit ice wait"2" lucid from"gh-r" lbin'!./*/bin/nvim -> nvim' for neovim/neovim
+  zinit wait"2" lucid from"gh-r" lbin'!./*/bin/nvim -> nvim' for neovim/neovim
 
   # }}}
 
