54f1beb40bbcc07fd142eedb53d7271f05787d77
xztaityozx <taityonohibi@gmail.com>
2023-09-25 16:34:58 +0900

    不要なチェックを削除

--- config/zsh/zinit.main.zsh
+++ config/zsh/zinit.main.zsh
@@ -1,7 +1,5 @@
 # zinit
 
-type zip tar curl wget git unzip &> /dev/null || return 127;
-
 [[ -f "$ZDOTDIR/.zinit/bin/zinit.zsh" ]] && source "$ZDOTDIR/.zinit/bin/zinit.zsh"
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
