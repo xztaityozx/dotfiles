eab1369947bd18d98d703f194eafc491f347deaf
xztaityozx <taityonohibi@gmail.com>
2025-04-10 10:54:45 +0900

    weztermの設定もdotfiles化

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require('wezterm')
+
+return {
+  font = wezterm.font("HackGen Console NF"),
+  color_scheme = 'iceberg-dark',
+  font_size = 18,
+  use_ime = true,
+  hide_tab_bar_if_only_one_tab = true,
+  bold_brightens_ansi_colors = true,
+  window_padding = { top = 0, bottom = 0, left = 0, right = 0 },
+  keys = {
+    { key = "q",  mods = "CTRL",    action = wezterm.action { SendString = "\x11" } },
+    { key = "¥",  mods = "ALT|OPT", action = wezterm.action { SendString = "\\" } },
+    { key = "F3", mods = "SHIFT",   action = wezterm.action.Nop }
+  }
+}
