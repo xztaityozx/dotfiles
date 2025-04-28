17599fa399627a0526c308daeeeb692baf7440b1
xztaityozx <taityonohibi@gmail.com>
2025-04-28 15:59:18 +0900

    manでエスケープシーケンスが書かれてしまって困ったので

--- config/zsh/zinit.atload.zsh
+++ config/zsh/zinit.atload.zsh
@@ -1,5 +1,5 @@
 function _zinit_bat_atload() {
-  export MANPAGER="sh -c 'col -xb | bat -l man -p'"
+  export MANPAGER="sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
   export BAT_THEME=TwoDark
   alias cat="bat"
   unfunction $0
