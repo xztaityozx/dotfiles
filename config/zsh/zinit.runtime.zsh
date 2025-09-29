086dd108fe100d3b58b233aa114eb488898771f6
xztaityozx <taityonohibi@gmail.com>
2025-09-29 17:14:33 +0900

    ばいばいasdf

--- config/zsh/zinit.runtime.zsh
+++ config/zsh/zinit.runtime.zsh
@@ -21,25 +21,25 @@
 
   # asdf
   # {{{
-    function _zinit_asdf_atinit() {
-      export ASDF_CONFIG_FILE=$DOTFILES_PATH/config/asdf/asdfrc
-      unfunction $0
-    }
+    #function _zinit_asdf_atinit() {
+      #export ASDF_CONFIG_FILE=$DOTFILES_PATH/config/asdf/asdfrc
+      #unfunction $0
+    #}
 
-    function __zinit_asdf_atclone_atpull() {
-      ./asdf completion zsh > _asdf
-    }
+    #function __zinit_asdf_atclone_atpull() {
+      #./asdf completion zsh > _asdf
+    #}
 
-    zinit \
-      atinit'_zinit_asdf_atinit' \
-      atclone"__zinit_asdf_atclone_atpull" atpull"%atclone" \
-      atload'export PATH=$HOME/.asdf/shims:$PATH' \
-      from"gh-r" \
-      completions \
-      silent \
-      light-mode\
-      nocompile\
-      lbin'!asdf' for @asdf-vm/asdf 
+    #zinit \
+      #atinit'_zinit_asdf_atinit' \
+      #atclone"__zinit_asdf_atclone_atpull" atpull"%atclone" \
+      #atload'export PATH=$HOME/.asdf/shims:$PATH' \
+      #from"gh-r" \
+      #completions \
+      #silent \
+      #light-mode\
+      #nocompile\
+      #lbin'!asdf' for @asdf-vm/asdf 
   # }}}
 
 # }}}
