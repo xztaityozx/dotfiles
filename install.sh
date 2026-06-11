640a358be5247e389d442ede4ac3629562c35022
xztaityozx <taityonohibi@gmail.com>
2026-06-11 16:14:57 +0900

    install時のaptを削除

--- install.sh
+++ install.sh
@@ -5,13 +5,6 @@ echo 'export ZDOTDIR=$HOME/.config/zsh && source $ZDOTDIR/.zshenv' >> $HOME/.zsh
 
 export ZDOTDIR=$HOME.config/zsh
 
-type apt-get &> /dev/null && {
-  echo debian/ubuntu
-
-  sudo apt update && sudo apt upgrade -y
-  sudo apt install -y build-essential wget curl git moreutils zip unzip tar zlib1g zlib1g-dev libssl-dev libbz2-dev libsqlite3-dev liblzma-dev libffi-dev libreadline-dev tk-dev clang lld
-}
-
 mkdir -p $HOME/.local/share/nvim/site/autoload
 
 mkdir -p $HOME/.config
